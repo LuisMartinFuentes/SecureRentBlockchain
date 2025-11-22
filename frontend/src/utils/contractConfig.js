e872c9c1deb1201f474f87a1a6840f3ba1b53f4a
LuisMartinFuentesAcosta <l21020657@veracruz.tecnm.mx>
2025-11-22 11:05:00 -0600

    Limpieza de residuos

--- frontend/src/utils/contractConfig.js
+++ frontend/src/utils/contractConfig.js
@@ -1,4 +1,4 @@
 import abi from "./RentSecureABI.json";
 
-export const contractAddress = "0x48fE1bB592d27aF903126B03a7dcEA2881c7c463"; // dirección temporal
+export const contractAddress = "0x48fE1bB592d27aF903126B03a7dcEA2881c7c463";
 export const contractABI = abi;
